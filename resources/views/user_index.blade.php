f9b139b6e14b780dcf530cf9a2ba860d45dffcd4
JAPACOM\okawa_r2 <okawa_r2@japacom.co.jp>
2018-09-10 10:14:42 +0900

    コミット

--- resources/views/user_index.blade.php
+++ resources/views/user_index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="col-md-12">
       <div class="card">
         <div class="card-header">社員情報編集</div>
       </div>
