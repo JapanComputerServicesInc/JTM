65ac08d6948ca80dbdaffd5ca4fbf36b0c6b8558
JAPACOM\okawa_r2 <okawa_r2@japacom.co.jp>
2018-09-10 10:33:30 +0900

    ｒ

--- resources/views/user_index.blade.php
+++ resources/views/user_index.blade.php
@@ -6,7 +6,12 @@
   <div class="row justify-content-center">
     <div class="col-md-12">
       <div class="card">
-        <div class="card-header">社員情報編集</div>
+        <div class="card-header">利用状況登録画面</div>
+        <div class="card-body">
+          <div class="form-group">
+            <label for="employeeNo">ホスト名</label>
+          </div>
+        </div>
       </div>
     </div>
   </div> 
