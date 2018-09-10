7bfe0ee138c41ab03f441934a7b1a1a255a68c0a
JAPACOM\hoshino_h <hoshino_h@japacom.co.jp>
2018-09-10 13:31:52 +0900

    端末情報一覧　コミット

--- resources/views/manager_index.blade.php
+++ resources/views/manager_index.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="content-panel">
+  <div class=" add-task-row">
+      <a class="btn btn-success btn-sm pull-right" href="#">新規登録</a>
+
+  </div>
+    <div class="content-panel">
     <table class="table table-striped table-advance table-hover">
-        <h4><i class="fa fa-angle-right"></i> 端末情報一覧</h4>
+        <h4><i class="fa fa-angle-right"></i> 端末情報一覧</i></h4>
         <hr>
         <thead>
             <tr>
                 <th><i class="fa fa-bullhorn"></i> id</th>
+                <th><i class="fa fa-bullhorn"></i> 利用状況</th>
                 <th class="hidden-phone"><i class="fa fa-question-circle"></i> ホスト名</th>
                 <th><i class="fa fa-bookmark"></i> 氏名</th>
                 <th><i class=" fa fa-edit"></i> 保管場所</th>
@@ -21,33 +26,32 @@
         </thead>
         <tbody>
             <tr>
-                <td><a href="basic_table.html#">Company Ltd</a></td>
-                <td class="hidden-phone">Lorem Ipsum dolor</td>
-                <td>12000.00$ </td>
-                <td><span class="label label-info label-mini">Due</span></td>
-                <td>
-                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                    <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                </td>
+                <td><a href="#">1</a></td>
+                <td><span class="label label-success label-mini">使用中</span></td>
+                <td class="hidden-phone"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr>
-                <td>
-                    <a href="basic_table.html#">
-                                    Dashgum co
-                                </a>
-                </td>
-                <td class="hidden-phone">Lorem Ipsum dolor</td>
-                <td>17900.00$ </td>
-                <td><span class="label label-warning label-mini">Due</span></td>
-                <td>
-                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                    <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                </td>
+                <td><a href="#">2</a></td>
+                <td><span class="label label-danger label-mini">未使用</span></td>
+                <td class="hidden-phone"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
         </tbody>
     </table>
+
 </div>
 <!-- /content-panel -->
 @endsection
