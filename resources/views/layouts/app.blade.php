a621ccb6e496f0104c776d19f87d157d688c73b4
JAPACOM\okawa_r2 <okawa_r2@japacom.co.jp>
2018-09-10 10:23:18 +0900

    コミット

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -224,7 +224,7 @@
                     <!-- sidebar menu start-->
                     <ul class="sidebar-menu" id="nav-accordion">
 
-                        <p class="centered"><a href="profile.html"><img src="{{asset('images/logo.png')}}" class="img-circle" width="60"></a></p>
+                        <p class="centered"><a href="profile.html"><img src="{{asset('images/logo.jpg')}}" class="img-circle" width="60"></a></p>
                         <h5 class="centered">Welcome To JTM!!</h5>
 
                         <li class="mt">
