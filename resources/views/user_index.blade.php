5993e2dd5a8f4cce1ad22314edcd682143fad11d
JAPACOM\okawa_r2 <okawa_r2@japacom.co.jp>
2018-09-12 16:03:03 +0900

    メニューバー

--- resources/views/user_index.blade.php
+++ resources/views/user_index.blade.php
@@ -1,48 +1,103 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="wrapper">
-  <h3>JCS　Terminal　Management　</h3>
-    <div class="row justify-content-center">
-      <div class="col-md-12">
-        <div class="card">
-          <div class="card-header"style="padding-top:15px;padding-left:15px;">新規社員登録</div>
+<section id="content">
+                <section class="wrapper">
+                     
+
+
+<div class="row mt">
+    <div class="col-lg-12">
+        <div class="form-panel">
+            <h4 class="mb">検索</h4>
             <hr>
-              <div class="card-body">
-                @csrf
-                  <div class="form-group">
-                    <div class="col-md-3">
-                      <label for="employee_no">社員氏名</label>
+            <form class="form-horizontal style-form" method="get">
+                <div class="form-group">
+                    <label class="col-sm-5 col-md-5 col-lg-1 control-label">氏名</label>
+                    <div class="col-lg-3">
+                        <input type="text" class="form-control">
                     </div>
-                    <div class="col-md-9">
-                      <input type="text" class="form-control">
-                      <span class="text-danger"></span>
+                    <label class="col-md-1 control-label">保管場所</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
                     </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-md-3"style="padding-top:20px;">
-                      <label for="employee_no">部署</label>
+                    <label class="col-md-1 control-label">OS</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
                     </div>
-                    <div class="col-md-9"style="padding-top:20px;">
-                      <input type="text" class="form-control">
-                      <span class="text-danger"></span>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-1 col-xs-12 control-label">CPU</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
                     </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-md-3"style="padding-top:20px;">
-                      <label for="employee_no">保管場所</label>
+                    <label class="col-md-1 col-xs-12 control-label">Office</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
                     </div>
-                    <div class="col-md-9"style="padding-top:20px;">
-                      <input type="text" class="form-control">
-                      <span class="text-danger"></span>
+                    <label class="col-md-1 col-xs-12 control-label">メモリ</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
                     </div>
-                  </div>
-              </div>  
-          </div>
-        </div>
-      </div>
-    </div>
-</section>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-1 col-xs-12 control-label">HDD/SSD</label>
+                    <div class="col-lg-3">
+                        <select class="form-control">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="checkbox-inline">
+                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+                    使用中
+                  </label>
+                  <label class="checkbox-inline">
+                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+                    未使用
+                  </label>
+                  <label class="checkbox-inline">
+                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option2">
+                    全て
+                  </label>
+                </div>
+
 @endsection
 
 
