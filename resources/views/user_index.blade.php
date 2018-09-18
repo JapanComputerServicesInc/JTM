e75bfbe1d078eb734b4771fcecd52402a2bde17f
JAPACOM\okawa_r2 <okawa_r2@japacom.co.jp>
2018-09-18 13:29:28 +0900

    結果にコミット　ライザップ

--- resources/views/user_index.blade.php
+++ resources/views/user_index.blade.php
@@ -2,71 +2,70 @@
 
 @section('content')
 <section id="content">
-                <section class="wrapper">
+ <section class="wrapper">
                      
 
 
 <div class="row mt">
-    <div class="col-lg-12">
-        <div class="form-panel">
-            <h4 class="mb">検索</h4>
-            <hr>
-            <form class="form-horizontal style-form" method="get">
-                <div class="form-group">
-                    <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">社員名</label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">部署</label>
-                    <div class="col-md-8">
-                        <select class="form-control">
-                            <option>SD事業部</option>
-                            <option>SD事業部2</option>
-                            <option>SD事業部3</option>
-                            <option>SD事業部4</option>
-                            <option>SD事業部5</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">保管場所</label>
+  <div class="col-lg-12">
+    <div class="form-panel">
+      <h4 class="mb">利用状況登録画面</h4>
+        <hr>
+          <form class="form-horizontal style-form" method="get">
+            <div class="form-group">
+              <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">社員名</label>
                 <div class="col-md-8">
-                        <input type="text" class="form-control">
-                    </div>
+                  <input type="text" class="form-control">
                 </div>
-                <div class="form-group">
-
-                <div class="radio">
-                      <div class="row">
-                      <div class="col-md-4">
-                        <p  style="padding-left:136px;padding-bottom:30px;font-size:1.5em;text-align:left;letter-spacing:1.3px;">利用状況</p>
-                        </div>
-                        <div class="col-md-8">
-                       <label style="padding-left: 28px;">
-                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+            </div>
+            <div class="form-group">
+              <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">部署</label>
+                <div class="col-md-8">
+                  <select class="form-control">
+                    <option>SD事業部</option>
+                    <option>SD事業部2</option>
+                    <option>SD事業部3</option>
+                    <option>SD事業部4</option>
+                    <option>SD事業部5</option>
+                  </select>
+                </div>
+             </div>
+             <div class="form-group">
+               <label class="col-md-4 control-label" style="font-size:1.5em;text-align:center;letter-spacing:1.3px;">保管場所</label>
+                 <div class="col-md-8">
+                 <input type="text" class="form-control">
+                 </div>
+             </div>
+             <div class="form-group">
+               <div class="radio">
+                 <div class="row">
+                   <div class="col-md-4">
+                     <p  style="padding-left:136px;padding-bottom:30px;font-size:1.5em;text-align:left;letter-spacing:1.3px;">利用状況</p>
+                    </div>
+                    <div class="col-md-8">
+                      <label style="padding-left: 28px;">
+                      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
                         使用中
-                       </label>
-                        <label style="padding-left: 85px;">
+                      </label>
+                      <label style="padding-left: 85px;">
                         <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
                         未使用
-                        </label>
-                        <label style="padding-left: 85px;">
-                        <input type="radio" name="optionsRadios" id="optionsRadios3" value="option2">
+                      </label>
+                      <label style="padding-left: 85px;">
+                      <input type="radio" name="optionsRadios" id="optionsRadios3" value="option2">
                         全て
-                        </label>
-                        </div>
+                      </label>
                     </div>
-                  </div>
                 </div>
-                <div class="form-group">
-                <div class="row">
-                        <div class="col-md-12" style="text-align:center;">                
-                        <button type="submit" class="btn btn-primary btn-round">登録</button>
-</div>
-</div>
-                </div>  
+            </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-md-12" style="text-align:center;">                
+                <button type="submit" class="btn btn-primary btn-round">登録</button>
+                </div>
+              </div>
+            </div>  
     </section>
 </section>
 
