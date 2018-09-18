4ad01b36e84075ebfc26a14b7b308a68320d9fa7
JAPACOM\nishitahara_a <nishitahara_a@japacom.co.jp>
2018-09-18 17:36:35 +0900

    新規端末　途中

--- app/Http/Controllers/AdminTerminalNewController.php
+++ app/Http/Controllers/AdminTerminalNewController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\TerminalCreateRequest;
+use App\Http\Requests\TerminalCreateRequest;
 use App\Employee;
 use App\Depository;
 use App\TerminalInformation;
@@ -11,6 +11,7 @@ use App\Memory;
 use App\Hdd;
 use App\Os;
 use App\OfficeInformation;
+use App\TerminalManagement;
 
 class AdminTerminalNewController extends Controller
 {
@@ -38,14 +39,15 @@ class AdminTerminalNewController extends Controller
     */
     public function create(TerminalCreateRequest $request)
     {
+        $status = $request->input('status');
         $pc_name = $request->input('pc_name');
         $approval_no = $request->input('approval_no');
-        $name = $request->input('employee_name');
-        $department = $request->input('department');
+        $employee_name = $request->input('employee_name');
         $depository = $request->input('depository');
         $producer = $request->input('producer');
         $product_name = $request->input('product_name');
         $serial_no = $request->input('serial_no');
+        $model_name = $request->input('model_name');
         $cpu = $request->input('cpu');
         $memory = $request->input('memory');
         $hdd = $request->input('hdd');
@@ -54,8 +56,32 @@ class AdminTerminalNewController extends Controller
         $memo = $request->input('memo');
         $qr_code = $request->input('qr_code');
 
+        $terminal_management = TerminalManagement::create([
+            'status_id' => $status,
+            'pc_name' => $pc_name,
+            'approval_no' => $approval_no,
+            'employees_id' => $employee_name,
 
 
+                'department' => $department,
+
+            'depositories_id' => $depository,
+
+            'producer' => $producer,
+            'product_name' => $product_name,
+
+            'serial_no' => $serial_no,
+            'model_name' => $model_name,
+            'cpu_id' => $cpu,
+            'memories_id' => $memory,
+            'hdd_id' => $hdd,
+            'os_id' => $os,
+            'office_info_id' => $office_info,
+            'memo' => $memo,
+            'qr_code' => $qr_code,
+        ]);
+
+     return redirect()->route('terminal_create', ['id'=>$terminal_management->id]);
 
     }
 }
--- resources/views/terminal_new.blade.php
+++ resources/views/terminal_new.blade.php
@@ -13,10 +13,10 @@
                     <label class="col-lg-2 col-sm-2 control-label"><i class="fa fa-check-square"></i>利用状況</label>
                     <div class="radio col-sm-10">
                         <label>
-                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>使用中
+                            <input type="radio" name="status" id="optionsRadios1" value="{{ old('status') }}" checked>使用中
                         </label>
                         <label>
-                            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">未使用
+                            <input type="radio" name="status" id="optionsRadios2" value="{{ old('status') }}">未使用
                         </label>
                     </div>
                 </div>
@@ -24,21 +24,21 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-laptop"></i>ホスト名</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="pc_name">
+                        <input type="text" class="form-control" name="pc_name" value="{{ old('pc_name') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-list-ol"></i>稟議No</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="approval_no">
+                        <input type="text" class="form-control" name="approval_no" value="{{ old('approval_no') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-user"></i>氏名</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="employee_name">
+                        <select class="form-control" name="employee_name" value="{{ old('employee_name') }}">
                             <option>-</option>
                             @foreach($employees as $employee)
                                 <option value="{{$employee->id}}">{{$employee->name}}</option>
@@ -50,14 +50,14 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-building"></i>部門</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="department">
+                        <input type="text" class="form-control" name="department" value="{{ old('department') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-save"></i>保管場所</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="depository">
+                        <select class="form-control" name="depository" value="{{ old('depository') }}">
                             <option>-</option>
                             @foreach($depositories as $depository)
                               <option value="{{$depository->id}}">{{$depository->depository}}</option>
@@ -69,7 +69,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-gavel"></i>メーカー名</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="producer">
+                        <select class="form-control" name="producer" value="{{ old('producer') }}">
                         <option>-</option>
                         @foreach($terminal_info as $t_i)
                             <option value="{{$t_i->id}}">{{$t_i->producer}}</option>
@@ -81,7 +81,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-desktop"></i>製品名</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="product_name">
+                        <select class="form-control" name="product_name" value="{{ old('product_name') }}">
                         <option>-</option>
                         @foreach($terminal_info as $t_i)
                             <option value="{{$t_i->id}}">{{$t_i->name}}</option>
@@ -94,21 +94,28 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-list-ol"></i>シリアルナンバー</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="serial_no">
+                        <input type="text" class="form-control" name="serial_no" value="{{ old('serial_no') }}">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label"><i class="fa fa-list-ol"></i>モデル名</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="model_name" value="{{ old('model_name') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-brain"></i>CPU</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="cpu">
+                        <input type="text" class="form-control" name="cpu" value="{{ old('cpu') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-save"></i>メモリ</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="memory">
+                        <select class="form-control" name="memory" value="{{ old('memory') }}">
                         <option>-</option>
                         @foreach($memories as $memory)
                             <option value="{{$memory->id}}">{{$memory->memory}}</option>
@@ -120,7 +127,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-edit"></i>HDD</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="hdd">
+                        <select class="form-control" name="hdd" value="{{ old('hdd') }}">
                         <option>-</option>
                         @foreach($hdd as $h)
                             <option value="{{$h->id}}">{{$h->hdd}}</option>
@@ -132,7 +139,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fab fa-apple"></i>OS</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="os">
+                        <select class="form-control" name="os" value="{{ old('os') }}">
                         <option>-</option>
                         @foreach($os as $o)
                             <option value="{{$o->id}}">{{$o->os}}</option>
@@ -144,7 +151,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fab fa-windows"></i>Office</label>
                     <div class="col-sm-10">
-                        <select class="form-control" name="office_info">
+                        <select class="form-control" name="office_info" value="{{ old('office_info') }}">
                             <option>-</option>
                             @foreach($office_info as $oi)
                               <option value="{{$oi->id}}">{{$oi->name}}</option>
@@ -156,14 +163,14 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-edit"></i>メモ</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="memo">
+                        <input type="text" class="form-control" name="memo" value="{{ old('memo') }}">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label class="col-sm-2 control-label"><i class="fa fa-envelope"></i>QRコードURL</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" name="qr_code">
+                        <input type="text" class="form-control" name="qr_code" value="{{ old('qr_code') }}">
                     </div>
                 </div>
 
