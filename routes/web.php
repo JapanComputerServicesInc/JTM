84bfcd7083f8e1ace39da29e4d6143bc66f36951
JAPACOM\hoshino_h <hoshino_h@japacom.co.jp>
2018-10-22 17:34:52 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -37,42 +37,42 @@ Route::group(['middleware' => 'auth'], function () {
 
       Route::prefix('management')->group(function () {
           //cpu管理
-          Route::get('/cpu/list','CpuManagementController@index')->name('cpu_index');
+          Route::get('/cpu','CpuManagementController@index')->name('cpu_index');
           Route::get('/cpu/new','CpuManagementController@new')->name('cpu_new');
           Route::post('/cpu/create','CpuManagementController@create')->name('cpu_create');
           Route::get('/cpu/{id}/edit','CpuManagementController@edit')->name('cpu_edit');
           Route::patch('/cpu/{id}','CpuManagementController@update')->name('cpu_update');
           Route::delete('/cpu/{id}','CpuManagementController@destroy')->name('cpu_destroy');
           //部署管理
-          Route::get('/department/list','DepartmentManagementController@index')->name('department_index');
+          Route::get('/department','DepartmentManagementController@index')->name('department_index');
           Route::get('/department/new','DepartmentManagementController@new')->name('department_new');
           Route::post('/department/create','DepartmentManagementController@create')->name('department_create');
           Route::get('/department/{id}/edit','DepartmentManagementController@edit')->name('department_edit');
           Route::patch('/department/{id}','DepartmentManagementController@update')->name('department_update');
           Route::delete('/department/{id}','DepartmentManagementController@destroy')->name('department_destroy');
           //保管場所管理
-          Route::get('/depository/list','DepositoryManagementController@index')->name('depository_index');
+          Route::get('/depository','DepositoryManagementController@index')->name('depository_index');
           Route::get('/depository/new','DepositoryManagementController@new')->name('depository_new');
           Route::post('/depository/create','DepositoryManagementController@create')->name('depository_create');
           Route::get('/depository/{id}/edit','DepositoryManagementController@edit')->name('depository_edit');
           Route::patch('/depository/{id}','DepositoryManagementController@update')->name('depository_update');
           Route::delete('/depository/{id}','DepositoryManagementController@destroy')->name('depository_destroy');
           //社員管理
-          Route::get('/employee/list','EmployeeManagementController@index')->name('employee_index');
+          Route::get('/employee','EmployeeManagementController@index')->name('employee_index');
           Route::get('/employee/new','EmployeeManagementController@new')->name('employee_new');
           Route::post('/employee/create','EmployeeManagementController@create')->name('employee_create');
           Route::get('/employee/{id}/edit','EmployeeManagementController@edit')->name('employee_edit');
           Route::patch('/employee/{id}','EmployeeManagementController@update')->name('employee_update');
           Route::delete('/employee/{id}','EmployeeManagementController@destroy')->name('employee_destroy');
           //端末情報管理
-          Route::get('/terminal/list','TerminalManagementController@index')->name('terminal_info_index');
+          Route::get('/terminal','TerminalManagementController@index')->name('terminal_info_index');
           Route::get('/terminal/new','TerminalManagementController@new')->name('terminal_info_new');
           Route::post('/terminal/create','TerminalManagementController@create')->name('terminal_info_create');
           Route::get('/terminal/{id}/edit','TerminalManagementController@edit')->name('terminal_info_edit');
           Route::patch('/terminal/{id}','TerminalManagementController@update')->name('terminal_info_update');
           Route::delete('/terminal/{id}','TerminalManagementController@destroy')->name('terminal_info_destroy');
           //office情報管理
-          Route::get('/office/list','OfficeManagementController@index')->name('office_index');
+          Route::get('/office','OfficeManagementController@index')->name('office_index');
           Route::get('/office/new','OfficeManagementController@new')->name('office_new');
           Route::post('/office/create','OfficeManagementController@create')->name('office_create');
           Route::get('/office/{id}/edit','OfficeManagementController@edit')->name('office_edit');
@@ -86,7 +86,7 @@ Route::get('/user_register/{pc_name}/edit', 'UserRegisterController@edit')->name
 Route::put('/user_register/{pc_name}','UserRegisterController@update')->name('user_register_update');
 
 // 一覧
-Route::get('/terminal/list','AdminTerminalIndexController@index')->name('terminal_index');
+Route::get('/terminal','AdminTerminalIndexController@index')->name('terminal_index');
 Route::post('/terminal/list/search','AdminTerminalIndexController@search')->name('terminal_search');
 
 //端末詳細
